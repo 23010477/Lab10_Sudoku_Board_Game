1518c1388a2ba36a8be84d108dc96d85912ab0a3
Ak <135825619+CozyMellow@users.noreply.github.com>
2025-12-16 13:27:12 +0200

    added two constructors to create a game
    one to create it from a 2d array and one from a SudokuBoard obj

--- src/main/java/Backend/BoardMethods/SudokuGame.java
+++ src/main/java/Backend/BoardMethods/SudokuGame.java
@@ -3,4 +3,26 @@ package Backend.BoardMethods;
 public class SudokuGame {
     private SudokuBoard sudokuBoard;
     private int emptyCells;
+
+    public SudokuGame(SudokuBoard sudokuBoard){
+        this.sudokuBoard = sudokuBoard;
+    }
+
+    public SudokuGame(int[][] sudokuBoard){// extra 3a4an yeb2a 3ndoko tare2tean n3mel game
+        this.sudokuBoard = new SudokuBoard(sudokuBoard);
+
+    }
+
+    public int emptyCellCounter(){
+        int count = 0;
+        return count;
+    }
+
+
+
+
+
+
+
+
 }
