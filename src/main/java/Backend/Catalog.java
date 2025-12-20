e49cf68af6c074e2a7303fe00101293cb5ebe926
Mohamed <mohamedwalid.elkholy@gmail.com>
2025-12-20 22:41:08 +0200

    Implemented catalog class

--- src/main/java/Backend/Catalog.java
+++ src/main/java/Backend/Catalog.java
@@ -4,6 +4,15 @@ public class Catalog {
     private boolean currentGame;
     private boolean modes;
 
+public Catalog(boolean currentGame, boolean modes){
+    this.currentGame=currentGame;
+    this.modes=modes;
+}
 
-
+public boolean hasCurrentGame(){
+    return currentGame;
+}
+public boolean hasModes(){
+    return modes;
+}
 }
