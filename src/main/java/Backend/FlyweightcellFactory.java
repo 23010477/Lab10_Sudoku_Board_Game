f1a319429bdaa10691e45ecaa87600bc0acde675
nader8757634 <naderking9968@gmail.com>
2025-12-21 13:07:38 +0200

    storage the empty cells possibilities

--- src/main/java/Backend/FlyweightcellFactory.java
+++ src/main/java/Backend/FlyweightcellFactory.java
@@ -1,5 +1,17 @@
 package Backend;
 
-public class FlyweightcellFactory {
+public final class FlyweightcellFactory {
 
+    private static final Flyweightcell[] emptyCells = new Flyweightcell[5];
+    private static int index = 0;
+
+    public static Flyweightcell getCell(int row, int col) {
+        if (index >= 5) {
+            throw new IllegalStateException("Only 5 flyweights allowed");
+        }
+        
+        emptyCells[index] = new Flyweightcell(row, col);
+        return emptyCells[index++];
+    }
+    
 }
