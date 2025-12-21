a283045629cc57a57ee7fae6e3724f41d021c704
nader8757634 <naderking9968@gmail.com>
2025-12-21 12:58:07 +0200

    Flyweight cell prepared

--- src/main/java/Backend/Flyweightcell.java
+++ src/main/java/Backend/Flyweightcell.java
@@ -1,4 +1,23 @@
+package Backend;
+
 
 public class Flyweightcell {
-
+	private final int row;
+	private final int col;
+	
+	public Flyweightcell(int row, int col) {
+		this.row=row;
+		this.col=col;
+		
+	}
+	
+	public int getrow() {
+		return row;
+	}
+	
+	public int getcol() {
+		return col ;
+	}
+	
 }
+
